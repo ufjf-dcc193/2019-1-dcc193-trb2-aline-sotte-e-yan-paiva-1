129826f6b6d58a2b25cdc09352ba49a844394bcf
YanPaiva <gckael@gmail.com>
2019-06-11 19:30:31 -0300

    Criando projeto

---
+++ src/test/java/br/ufjf/dcc193/Trabalho_Aline_e_Yan/TrabalhoAlineEYanApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc193.Trabalho_Aline_e_Yan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoAlineEYanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
