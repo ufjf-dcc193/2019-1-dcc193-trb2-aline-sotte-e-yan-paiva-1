b33cab8c0558561d529ecbaedcc7fc387d5081af
YanPaiva <gckael@gmail.com>
2019-06-15 01:10:46 -0300

    criando tela inicial e query de buscano banco

--- src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/AvaliadorRepositorio.java
+++ src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/AvaliadorRepositorio.java
@@ -1,10 +1,18 @@
-package  br.ufjf.dcc193.trabalho_Aline_e_Yan;
+package br.ufjf.dcc193.trabalho_Aline_e_Yan;
 
 import br.ufjf.dcc193.trabalho_Aline_e_Yan.*;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AvaliadorRepositorio extends JpaRepository<Avaliador, Long>{
+    
+    @Query("SELECT id FROM Avaliador a WHERE a.email =:email and a.codigoAcesso =:codigoAcesso")
+    Long getIdByEmailAndSenha(@Param("email") String email,
+     @Param("codigoAcesso") String codigoAcesso);
 
 }
--- src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/HomeController.java
+++ src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/HomeController.java
@@ -1,18 +1,36 @@
 package br.ufjf.dcc193.trabalho_Aline_e_Yan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * HomeController
  */
 @Controller
 public class HomeController {
+    @Autowired
+    AvaliadorRepositorio avdRepo;
     @RequestMapping("/")
     @ResponseBody
-    public String index(){
-        return "<h1> Bem Vindo</h1>";
+
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+
+    @PostMapping(value="/login.html")
+    public ModelAndView login(String email, String senha){
+            ModelAndView mv = new ModelAndView();
+            
+            Avaliador avaliador = avdRepo.getOne(
+                avdRepo.getIdByEmailAndSenha(email, senha)); 
+            mv.setViewName("redirect:listar.html");
+            return mv;
     }
     
 }
