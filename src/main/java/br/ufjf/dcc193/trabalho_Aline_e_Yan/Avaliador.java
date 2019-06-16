1b1b014f9be47d20028134c144ecfb71497b800b
Aline Sotte <alinedpaso@gmail.com>
2019-06-16 13:01:44 -0300

    Refatoração Modelo Avaliador

--- src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/Avaliador.java
+++ src/main/java/br/ufjf/dcc193/trabalho_Aline_e_Yan/Avaliador.java
@@ -8,12 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "tb_avaliador")
 public class Avaliador {
 
    @Id
